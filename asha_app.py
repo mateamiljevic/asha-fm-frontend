21990ddfed07e63c2bd26c78bd76bc28b03ff9d3
Matea Miljevic <mateamiljevic@Mateas-MacBook-Pro.local>
2023-03-13 15:11:01 +0000

    font

--- asha_app.py
+++ asha_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
+streamlit_style = """
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+			html, body, [class*="css"]  {
+			font-family: 'Roboto', STRETCH-SANS;
+			}
+			</style>
+			"""
+
 st.markdown("""#ASHA FM""")
 
+video_file = open('video-1626791370.mp4', 'rb')
+video_bytes = video_file.read()
 
-st.video(data, format="video/mp4", start_time=0)
+st.video(video_bytes)
