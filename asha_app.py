be34b59afadcd8c1f3fb39b373fbbee2b5cb70e7
Matea Miljevic <mateamiljevic@Mateas-MacBook-Pro.local>
2023-03-15 16:26:59 +0000

    frontend done

--- asha_app.py
+++ asha_app.py
@@ -7,23 +7,12 @@ st.set_page_config(layout="wide")
 
 
 ##HEADING
-col1, col2, col3, col4, col5 = st.columns(5)
-
-with col1:
-    st.write(' ')
-
-with col2:
-    image = Image.open('data/ASHAFMLOGO.PNG')
-    st.image(image, caption=None, width=650)
-
-with col3:
-    st.write(' ')
-
-with col4:
-    st.write(' ')
-
-with col5:
-    st.write(' ')
+file_ = open("data/movinglogo.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+unsafe_allow_html=True)
 
 
 
@@ -35,7 +24,7 @@ st.video(video_bytes)
 
 
 st.write("  ")
-st.write("  ")
+
 
 ##DESCRIPTION/BACKGROUND FORMAT
 st.markdown("""
@@ -48,9 +37,39 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+st.markdown("""
+<style>
+.big-font {
+    font-size:40px;
+    font-weight: bold;
+    text-align: center;
+}
+</style>
+""", unsafe_allow_html=True)
+
 ##DESCRIPTION/BACKGROUND TEXT
 with st.container():
-   st.markdown('<p class="mid-font">SOLIDARITY & SELECTAS brought by @ashasoundsystem - coming soon.</p>', unsafe_allow_html=True)
+   st.markdown('<p class="big-font">SOLIDARITY & SELECTAS brought to you by ASHA FM</p>', unsafe_allow_html=True)
+st.write("  ")
+st.write("  ")
+
+st.markdown("""
+<style>
+.desc-font {
+    font-size:25px;
+    font-weight: thin;
+}
+</style>
+""", unsafe_allow_html=True)
+
+with st.expander("READ MORE ABOUT OUR AUDIOVISUAL INTERACTIVE PROJECT:"):
+    st.markdown('<p class="desc-font">Asha FM is a revolutionary project that creates cutting-edge audioreactive visual art with the power of AI technology. More than just a platform - it is a way to share your music with the world in a truly immersive way.</p>', unsafe_allow_html=True)
+    st.write(" ")
+    st.markdown('<p class="desc-font">With Asha FM, you can unleash your creativity and bring your music to life like never before. Our AI system generates mind-blowing visuals that pulse with the beat of your music, creating an electrifying experience for you and your audience. And with our radio station releasing new DJ set music videos every Friday, you will never run out of fresh content to explore.</p>', unsafe_allow_html=True)
+    st.write(" ")
+    st.markdown('<p class="desc-font">But Asha FM is more than just a tool for artists. It is a movement. We are all about SOLIDARITY & SELECTAS, bringing people together through the power of art and music. Whether you are a seasoned pro or just starting out, Asha FM is your ticket to a world of endless artistic possibilities.</p>', unsafe_allow_html=True)
+    st.write(" ")
+    st.markdown('<p class="desc-font">So what are you waiting for? Join the revolution and start creating with Asha FM today. The countdown to your next mind-bending artistic adventure starts now.</p>', unsafe_allow_html=True)
 
 st.write("  ")
 st.write("  ")
@@ -114,23 +133,43 @@ with col4:
     unsafe_allow_html=True,
 )
 
+st.write("  ")
+st.write("  ")
 
 
+##DEMO INSTRUCTIONS
+st.markdown("""
+<style>
+.instr-font {
+    font-size:25px;
+    font-weight: thin;
+    color: #000000;
+    text-align: center;
+    background-color: #FFFFFF;
+    border-radius:2%;
+}
+</style>
+""", unsafe_allow_html=True)
 
+st.markdown('<p class="instr-font">TO START, UPLOAD YOUR MUSIC FILE IN MP3/WAV FORMAT.</p>', unsafe_allow_html=True)
 st.write("  ")
-st.write("  ")
-st.write("  ")
-st.write("  ")
+
 
 ##UPLOAD AUDIO BUTTON
 audio = st.file_uploader("UPLOAD YOUR MUSIC", type=["mp3, WAV, AIFF"])
 st.write("  ")
 st.write("  ")
 
+##DEMO INSTRUCTIONS- DOWNLOAD
+st.markdown('<p class="instr-font">NOW PRESS DOWNLOAD AND WATCH THE MAGIC UNFOLD.</p>', unsafe_allow_html=True)
+st.write("  ")
+
 ##PLACEHOLDER FOR IMAGE UPLOAD - STRETCH GOAL
 ##images = st.file_uploader("UPLOAD YOUR IMAGES", type="jpg, png")
 ##st.write("  ")
 ##st.write("  ")
+
+##DOWNLOAD BUTTON
 with open("data/video-1626791370.mp4", "rb") as file:
     btn = st.download_button(
             label="DOWNLOAD YOUR VIDEO",
@@ -142,6 +181,61 @@ with open("data/video-1626791370.mp4", "rb") as file:
 st.write("  ")
 st.write("  ")
 st.write("  ")
+
+
+
+
+##OUR WONDERFUL TAS
+st.markdown('<p class="mid-font">BIG THANK YOU FROM THE ASHA FM TEAM TO OUR WONDERFUL LE WAGON TAS</p>', unsafe_allow_html=True)
+st.write("  ")
+col1, col2, col3, col4 = st.columns(4)
+
+st.markdown("""
+<style>
+.header-font {
+    font-size:90px;
+    font-weight: bold;
+    text-align: center;
+}
+</style>
+""", unsafe_allow_html=True)
+
+with col1:
+    st.write("  ")
+
+with col2:
+   st.markdown('<p class="mid-font">CHARLOTTE</p>', unsafe_allow_html=True)
+   file_ = open("data/charlottegif.gif", "rb")
+   contents = file_.read()
+   data_url = base64.b64encode(contents).decode("utf-8")
+   file_.close()
+   st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
+
+with col3:
+   st.markdown('<p class="mid-font">LORCAN</p>', unsafe_allow_html=True)
+   file_ = open("data/lorcangif.gif", "rb")
+   contents = file_.read()
+   data_url = base64.b64encode(contents).decode("utf-8")
+   file_.close()
+   st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
+
+
+with col4:
+    st.write("  ")
+
+
+st.write("  ")
+st.write("  ")
+st.write("  ")
+st.write("  ")
+
+
 
 
 ##SOCIAL MEDIA HANDLE AND WEBSITE LINK
